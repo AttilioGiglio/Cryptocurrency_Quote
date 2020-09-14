4886fbabc9bf98de4173bd52167a05fc5bc6e72f
attiliogiglio <attilio.g93@gmail.com>
2020-09-14 18:59:11 -0300

    adding styled components to quote

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+    span {
+        font-weight:bold;
+    }
+`;
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight:bold;
+    }
+`
 
 const Quote = ({result}) => {
     if(Object.keys(result).length === 0) return null;
     console.log(result);
     return (
-        <div>
-            <p>Daily Price: <span>{result.PRICE}</span></p>
-            <p>Highest daily price: <span>{result.HIGHDAY}</span></p>
-            <p>Lowest daily price: <span>{result.LOWDAY}</span></p>
-            <p>24-hour price change: <span>{result.CHANGEPCT24HOUR}</span></p>
-            <p>Last update: <span>{result.LASTUPDATE}</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>Daily Price: <span>{result.PRICE}</span></Precio>
+            <Info>Highest daily price: <span>{result.HIGHDAY}</span></Info>
+            <Info>Lowest daily price: <span>{result.LOWDAY}</span></Info>
+            <Info>24-hour price change: <span>{result.CHANGEPCT24HOUR}</span></Info>
+            <Info>Last update: <span>{result.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
     )
 }
 
